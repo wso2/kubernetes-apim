4d3e24a8a0e1b822c4fbf047aeacfea1bf6e6187
Isuru Haththotuwa <isurulucky@gmail.com>
2017-07-17 15:27:29 +0530

    moving rsync Dockerfiles to patterns and adding rsync support for pattern-2

--- rsync/init.sh
+++ rsync/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-echo 'starting cron deamon'
-cron
 echo 'scheduling artifact sync cron job'
 crontab -u ${USER} ${USER_HOME}/artifact-sync-cron
+echo 'starting cron deamon'
+cron
 tailf ${USER_HOME}/cron/logs/artifact-sync-cron.log
