cbf342ae8954f35cd8ba6b73980e75e19817bc17
Isuru Haththotuwa <isurulucky@gmail.com>
2017-07-20 12:50:48 +0530

    minor corrections

--- rsync/init.sh
+++ rsync/init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-set -e
-echo 'scheduling artifact sync cron job'
-crontab -u ${USER} ${USER_HOME}/artifact-sync-cron
-echo 'starting cron deamon'
-cron
-tailf ${USER_HOME}/cron/logs/artifact-sync-cron.log
+set -x
+echo 'scheduling artifact sync cron job & starting cron deamon'
+crontab -u ${USER} ${USER_HOME}/artifact-sync-cron && cron \
+	&& tailf ${USER_HOME}/cron/logs/artifact-sync-cron.log
