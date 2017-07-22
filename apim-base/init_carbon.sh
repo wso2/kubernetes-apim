b71886828bc9b3216eed51906c5ddfd2c6690e72
Isuru Haththotuwa <isurulucky@gmail.com>
2017-07-22 17:18:30 +0530

    clustering related changes

--- apim-base/init_carbon.sh
+++ apim-base/init_carbon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 # Copy the backed up artifacts from ${HOME}/tmp/server/. Copying the initial artifacts to ${HOME}/tmp/server/ is done in the 
 # Dockerfile. This is to preserve the initial artifacts in a volume mount (the mounted directory can be empty initially). 
 # The artifacts will be copied to the CARBON_HOME/repository/deployment/server location before the server is started.
@@ -19,5 +20,18 @@ if [[ -d ${HOME}/tmp/carbon/ ]]; then
    cp -rf ${HOME}/tmp/carbon/* ${carbon_home}/
    rm -rf ${HOME}/tmp/carbon/
 fi
+
+# overwrite localMemberHost element value in axis2.xml with container ip
+export local_docker_ip=$(ip route get 1 | awk '{print $NF;exit}')
+axi2_xml_location=${carbon_home}/repository/conf/axis2/axis2.xml
+if [[ ! -z ${local_docker_ip} ]]; then
+   sed -i "s#<parameter\ name=\"localMemberHost\".*#<parameter\ name=\"localMemberHost\">${local_docker_ip}<\/parameter>#" "${axi2_xml_location}"
+   if [[ $? == 0 ]]; then
+      echo "Successfully updated localMemberHost with ${local_docker_ip}"
+   else
+      echo "Error occurred while updating localMemberHost with ${local_docker_ip}"
+   fi
+fi
+
 # Start the carbon server.
 ${carbon_home}/bin/wso2server.sh
