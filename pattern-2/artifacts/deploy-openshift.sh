f2076c07f7a8f06e46d10520133adbcde30ea6cb
Pubudu Gunatilaka <pubudu538@gmail.com>
2017-08-08 15:57:24 +0530

    Changing pattern-2

--- pattern-2/artifacts/deploy-openshift.sh
+++ pattern-2/artifacts/deploy-openshift.sh
@@ -15,35 +15,46 @@ sleep 20s
 # analytics
 echo 'deploying apim analytics ...'
 oc create -f apim-analytics/wso2apim-analytics-service.yaml
-oc create -f apim-analytics/wso2apim-analytics-deployment.yaml
+oc create -f apim-analytics/wso2apim-analytics-1-service.yaml
+oc create -f apim-analytics/wso2apim-analytics-2-service.yaml
+oc create -f apim-analytics/wso2apim-analytics-1-deployment.yaml
+sleep 30s
+oc create -f apim-analytics/wso2apim-analytics-2-deployment.yaml
 
 sleep 1m
 # apim volumes and services
-oc create -f apim-pubstore-tm-km/wso2apim-tm1-volume-claim.yaml
-oc create -f apim-pubstore-tm-km/wso2apim-tm2-volume-claim.yaml
+oc create -f apim-pubstore-tm/wso2apim-tm1-volume-claim.yaml
+oc create -f apim-pubstore-tm/wso2apim-tm2-volume-claim.yaml
 
 oc create -f apim-gateway/wso2apim-mgt-volume-claim.yaml
 oc create -f apim-gateway/wso2apim-worker-volume-claim.yaml
 
-oc create -f apim-pubstore-tm-km/wso2apim-service.yaml
-oc create -f apim-pubstore-tm-km/wso2apim-pubstore-tm-km-1-service.yaml
-oc create -f apim-pubstore-tm-km/wso2apim-pubstore-tm-km-2-service.yaml
+oc create -f apim-pubstore-tm/wso2apim-service.yaml
+oc create -f apim-pubstore-tm/wso2apim-pubstore-tm-1-service.yaml
+oc create -f apim-pubstore-tm/wso2apim-pubstore-tm-2-service.yaml
 
 oc create -f apim-gateway/wso2apim-worker-service.yaml
 oc create -f apim-gateway/wso2apim-sv-service.yaml
 oc create -f apim-gateway/wso2apim-pt-service.yaml
 oc create -f apim-gateway/wso2apim-manager-worker-service.yaml
 
-echo 'deploying apim pubstore-tm-km-1 ...'
-oc create -f apim-pubstore-tm-km/wso2apim-pubstore-tm-km-1-deployment.yaml
+oc create -f apim-km/wso2apim-km-service.yaml
+oc create -f apim-km/wso2apim-key-manager-service.yaml
 
-sleep 30s
-echo 'deploying apim pubstore-tm-km-2 ...'
-oc create -f apim-pubstore-tm-km/wso2apim-pubstore-tm-km-2-deployment.yaml
+echo 'deploying apim pubstore-tm-1 ...'
+oc create -f apim-pubstore-tm/wso2apim-pubstore-tm-1-deployment.yaml
+
+sleep 1m
+echo 'deploying apim pubstore-tm-2 ...'
+oc create -f apim-pubstore-tm/wso2apim-pubstore-tm-2-deployment.yaml
+
+sleep 1m
+echo 'deploying apim key manager...'
+oc create -f apim-km/wso2apim-km-deployment.yaml
 
 sleep 1m
 echo 'deploying apim manager-worker ...'
 oc create -f apim-gateway/wso2apim-manager-worker-deployment.yaml
-sleep 30s
+sleep 1m
 echo 'deploying apim worker ...'
 oc create -f apim-gateway/wso2apim-worker-deployment.yaml
