4b6137485fa3f14328980c3de544997fcc7549ef
Pubudu Gunatilaka <pubudu538@gmail.com>
2017-08-21 11:30:42 +0530

    Adding mysql dockerfile

--- base/build.sh
+++ base/build.sh
@@ -7,8 +7,10 @@ rsync_dir=$(cd "${this_dir}/rsync"; pwd)
 sshd_dir=$(cd "${this_dir}/sshd"; pwd)
 analytics_dir=$(cd "${this_dir}/analytics"; pwd)
 apim_dir=$(cd "${this_dir}/apim"; pwd)
+mysql_dir=$(cd "${this_dir}/mysql"; pwd)
 
 docker build -t wso2/rsync:1.0.0 $rsync_dir --squash
 docker build -t wso2/sshd:1.0.0 $sshd_dir --squash
 docker build -t wso2/wso2am:2.1.0 $apim_dir --squash
 docker build -t wso2/wso2am-analytics:2.1.0 $analytics_dir --squash
+docker build -t docker.wso2.com/apim-rdbms-kubernetes:2.1.0 $mysql_dir --squash
---
+++ base/mysql/scripts/mysql-statdbs.sql
@@ -0,0 +1,4 @@
+create database statdb;
+create database analyticseventstoredb;
+create database analyticprocesseddatastoredb;
+set global max_connections = 200;
