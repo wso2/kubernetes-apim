63f95cc14d835bdc2378cb305e7204f48033fe6b
Chamila de Alwis <chamila@apache.org>
2017-08-23 18:36:00 +0530

    Add analytics Dockerfile with improvements to existing ones

--- dockerfile/api-gateway/gateway.sh
+++ dockerfile/api-gateway/gateway.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Get MB HOST and PORT from env and update configs  
-if [[ ! -z "${ACTIVEMQ_SERVICE_HOST}" ]];then
-    if [[ ! -z "${ACTIVEMQ_SERVICE_PORT}" ]];then
-	find org/wso2/carbon/apimgt/gateway/ -type f -print0 | xargs -0 sed -i -e "s/localhost\:61616/${ACTIVEMQ_SERVICE_HOST}:${ACTIVEMQ_SERVICE_PORT}/g"
-    else
-        find org/wso2/carbon/apimgt/gateway/ -type f -print0 | xargs -0 sed -i -e "s/localhost\:61616/${ACTIVEMQ_SERVICE_HOST}:61616/g"
-    fi
-fi
+# TODO: set broker URL somehow
 
-bin/ballerina run service services.bsz
+/mnt/wso2apim-gateway-3.0.0-m6/bin/ballerina run service /mnt/wso2apim-gateway-3.0.0-m6/services.bsz
