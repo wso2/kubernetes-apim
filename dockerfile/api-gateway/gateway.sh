60691edd43b9e8418c6712282e317aa738f74a04
Chamila de Alwis <chamila@apache.org>
2017-09-04 11:35:33 +0530

    Change workdir from /mnt to /opt

--- dockerfile/api-gateway/gateway.sh
+++ dockerfile/api-gateway/gateway.sh
@@ -2,4 +2,4 @@
 
 # TODO: set broker URL somehow
 
-/mnt/wso2apim-gateway-3.0.0-m6/bin/ballerina run service /mnt/wso2apim-gateway-3.0.0-m6/services.bsz
+/opt/wso2apim-gateway-3.0.0-m6/bin/ballerina run service /opt/wso2apim-gateway-3.0.0-m6/services.bsz
