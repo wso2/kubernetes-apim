e41419907d6f658629b0a0757b515e4c0453fd0f
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2018-03-25 08:56:40 +0530

    Remove sudoers files and improve docker image size

--- base/analytics/init_carbon.sh
+++ base/analytics/init_carbon.sh
@@ -17,110 +17,116 @@
 # ------------------------------------------------------------------------
 
 set -e
-# The artifacts will be copied to the CARBON_HOME/repository/deployment/server location before the server is started.
-carbon_home=${HOME}/${WSO2_SERVER}-${WSO2_SERVER_VERSION}
-server_artifact_location=${carbon_home}/repository/deployment/server
-sudo /bin/change_ownership.sh
-if [[ -d ${HOME}/tmp/server/ ]]; then
+server_artifact_location=${WSO2_SERVER_HOME}/repository/deployment/server
+
+# Copy artifacts copied to ${USER_HOME}/tmp-server/ directory in the docker image build process
+# to ${WSO2_SERVER_HOME}/repository/deployment/server once the persistent volume is attached for the first time
+if [[ -d ${USER_HOME}/tmp-server/ ]]; then
    if [[ ! "$(ls -A ${server_artifact_location}/)" ]]; then
-      # There are no artifacts under CARBON_HOME/repository/deployment/server/; copy them.
-      echo "copying artifacts from ${HOME}/tmp/server/ to ${server_artifact_location}/ .."
-      cp -rf ${HOME}/tmp/server/* ${server_artifact_location}/
+      # There are no artifacts under ${WSO2_SERVER_HOME}/repository/deployment/server, copy them
+      echo "copying artifacts from ${USER_HOME}/tmp-server/ to ${server_artifact_location}/ ..."
+      cp -rf ${USER_HOME}/tmp-server/* ${server_artifact_location}/
    fi
-   rm -rf ${HOME}/tmp/server/
+   rm -rf ${USER_HOME}/tmp-server/
 fi
-if [[ -d ${HOME}/tmp/carbon/ ]]; then
-   echo "copying custom configurations and artifacts from ${HOME}/tmp/carbon/ to ${carbon_home}/ .."
-   cp -rf ${HOME}/tmp/carbon/* ${carbon_home}/
-   rm -rf ${HOME}/tmp/carbon/
+
+# Copy customizations done by user do the CARBON_HOME location
+if [[ -d ${USER_HOME}/tmp-carbon/ ]]; then
+   echo "copying custom configurations and artifacts from ${USER_HOME}/tmp-carbon/ to ${WSO2_SERVER_HOME}/ ..."
+   cp -rf ${USER_HOME}/tmp-carbon/* ${WSO2_SERVER_HOME}/
+   rm -rf ${USER_HOME}/tmp-carbon/
 fi
 
 # Copy ConfigMaps
-# Mount any ConfigMap to ${carbon_home}-conf location
-if [ -e ${carbon_home}-conf/bin/* ]
- then cp ${carbon_home}-conf/bin/* ${carbon_home}/bin/
+# Mount any ConfigMap to ${WSO2_SERVER_HOME}-conf location
+if [[ -d ${WSO2_SERVER_HOME}-conf/ ]]; then
+   echo "copying config maps from ${WSO2_SERVER_HOME}-conf to ${WSO2_SERVER_HOME}/ ..."
 fi
 
-if [ -e ${carbon_home}-conf/resources-security ]
- then cp ${carbon_home}-conf/resources-security/* ${carbon_home}/repository/resources/security/
+if [ -e ${WSO2_SERVER_HOME}-conf/bin/* ]
+ then cp ${WSO2_SERVER_HOME}-conf/bin/* ${WSO2_SERVER_HOME}/bin/
 fi
 
-if [ -e ${carbon_home}-conf/conf ]
- then cp ${carbon_home}-conf/conf/* ${carbon_home}/repository/conf/
+if [ -e ${WSO2_SERVER_HOME}-conf/resources-security ]
+ then cp ${WSO2_SERVER_HOME}-conf/resources-security/* ${WSO2_SERVER_HOME}/repository/resources/security/
 fi
 
-if [ -e ${carbon_home}-conf/conf-axis2 ]
- then cp ${carbon_home}-conf/conf-axis2/* ${carbon_home}/repository/conf/axis2/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf/* ${WSO2_SERVER_HOME}/repository/conf/
 fi
 
-if [ -e ${carbon_home}-conf/conf-datasources ]
- then cp ${carbon_home}-conf/conf-datasources/* ${carbon_home}/repository/conf/datasources/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-axis2 ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-axis2/* ${WSO2_SERVER_HOME}/repository/conf/axis2/
 fi
 
-if [ -e ${carbon_home}-conf/conf-identity ]
- then cp ${carbon_home}-conf/conf-identity/* ${carbon_home}/repository/conf/identity/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-datasources ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-datasources/* ${WSO2_SERVER_HOME}/repository/conf/datasources/
 fi
 
-if [ -e ${carbon_home}-conf/conf-tomcat ]
- then cp ${carbon_home}-conf/conf-tomcat/* ${carbon_home}/repository/conf/tomcat/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-identity ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-identity/* ${WSO2_SERVER_HOME}/repository/conf/identity/
 fi
 
-if [ -e ${carbon_home}-conf/conf-data-bridge ]
- then cp ${carbon_home}-conf/conf-data-bridge/* ${carbon_home}/repository/conf/data-bridge/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-tomcat ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-tomcat/* ${WSO2_SERVER_HOME}/repository/conf/tomcat/
 fi
 
-if [ -e ${carbon_home}-conf/conf-email ]
- then cp ${carbon_home}-conf/conf-email/* ${carbon_home}/repository/conf/email/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-data-bridge ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-data-bridge/* ${WSO2_SERVER_HOME}/repository/conf/data-bridge/
 fi
 
-if [ -e ${carbon_home}-conf/conf-etc ]
- then cp ${carbon_home}-conf/conf-etc/* ${carbon_home}/repository/conf/etc/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-email ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-email/* ${WSO2_SERVER_HOME}/repository/conf/email/
 fi
 
-if [ -e ${carbon_home}-conf/conf-multitenancy ]
- then cp ${carbon_home}-conf/conf-multitenancy/* ${carbon_home}/repository/conf/multitenancy/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-etc ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-etc/* ${WSO2_SERVER_HOME}/repository/conf/etc/
 fi
 
-if [ -e ${carbon_home}-conf/conf-security ]
- then cp ${carbon_home}-conf/conf-security/* ${carbon_home}/repository/conf/security/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-multitenancy ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-multitenancy/* ${WSO2_SERVER_HOME}/repository/conf/multitenancy/
 fi
 
-if [ -e ${carbon_home}-conf/conf-analytics ]
- then cp ${carbon_home}-conf/conf-analytics/* ${carbon_home}/repository/conf/analytics/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-security ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-security/* ${WSO2_SERVER_HOME}/repository/conf/security/
 fi
 
-if [ -e ${carbon_home}-conf/conf-analytics-spark ]
- then cp ${carbon_home}-conf/conf-analytics-spark/* ${carbon_home}/repository/conf/analytics/spark/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-analytics ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-analytics/* ${WSO2_SERVER_HOME}/repository/conf/analytics/
 fi
 
-if [ -e ${carbon_home}-conf/conf-cep ]
- then cp ${carbon_home}-conf/conf-cep/* ${carbon_home}/repository/conf/cep/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-analytics-spark ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-analytics-spark/* ${WSO2_SERVER_HOME}/repository/conf/analytics/spark/
 fi
 
-if [ -e ${carbon_home}-conf/conf-cep-domain-template ]
- then cp ${carbon_home}-conf/conf-cep-domain-template/* ${carbon_home}/repository/conf/cep/domain-template/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-cep ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-cep/* ${WSO2_SERVER_HOME}/repository/conf/cep/
 fi
 
-if [ -e ${carbon_home}-conf/conf-cep-storm ]
- then cp ${carbon_home}-conf/conf-cep-storm/* ${carbon_home}/repository/conf/cep/storm/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-cep-domain-template ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-cep-domain-template/* ${WSO2_SERVER_HOME}/repository/conf/cep/domain-template/
 fi
 
-if [ -e ${carbon_home}-conf/conf-template-manager ]
- then cp ${carbon_home}-conf/conf-template-manager/* ${carbon_home}/repository/conf/template-manager/domain-template/
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-cep-storm ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-cep-storm/* ${WSO2_SERVER_HOME}/repository/conf/cep/storm/
 fi
 
-# overwrite localMemberHost element value in axis2.xml with container ip
+if [ -e ${WSO2_SERVER_HOME}-conf/conf-template-manager ]
+ then cp ${WSO2_SERVER_HOME}-conf/conf-template-manager/* ${WSO2_SERVER_HOME}/repository/conf/template-manager/domain-template/
+fi
+
+# Overwrite localMemberHost element value in axis2.xml with container ip
 export local_docker_ip=$(ip route get 1 | awk '{print $NF;exit}')
 export  SPARK_LOCAL_IP=$local_docker_ip
-axi2_xml_location=${carbon_home}/repository/conf/axis2/axis2.xml
+axi2_xml_location=${WSO2_SERVER_HOME}/repository/conf/axis2/axis2.xml
 if [[ ! -z ${local_docker_ip} ]]; then
    sed -i "s#<parameter\ name=\"localMemberHost\".*#<parameter\ name=\"localMemberHost\">${local_docker_ip}<\/parameter>#" "${axi2_xml_location}"
    if [[ $? == 0 ]]; then
-      echo "Successfully updated localMemberHost with ${local_docker_ip}"
+      echo "successfully updated localMemberHost with ${local_docker_ip}"
    else
-      echo "Error occurred while updating localMemberHost with ${local_docker_ip}"
+      echo "error occurred while updating localMemberHost with ${local_docker_ip}"
    fi
 fi
 
-# Start the carbon server.
-${HOME}/${WSO2_SERVER}-${WSO2_SERVER_VERSION}/bin/wso2server.sh
+# Start the carbon server
+${WSO2_SERVER_HOME}/bin/wso2server.sh
