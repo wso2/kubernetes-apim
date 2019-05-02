2c0f2515e516af943c973bd43a5ac863e6749142
NishikaDeSilva <desilvanishika73@gmail.com>
2019-05-02 14:04:33 +0530

    Modify parameter parsing

--- simple/wso2am_open.sh
+++ simple/wso2am_open.sh
@@ -3302,15 +3302,15 @@ function progress_bar(){
               let "num_true=num_true+1"
           fi
       done
-      printf "\rProcessing WSO2 API Manager ... |"
+      printf "Processing WSO2 API Manager ... |"
 
-      printf "%-$((5 * ${length_total-1}))s| $(($num_true_const * 100/ $length_total))"; echo -en ' % '
+      printf "%-$((5 * ${length_total-1}))s| $(($num_true_const * 100/ $length_total))"; echo -en ' % \r'
 
-      printf "\rProcessing WSO2 API Manager ... |"
+      printf "Processing WSO2 API Manager ... |"
       s=$(printf "%-$((5 * ${num_true_const}))s" "H")
       echo -en "${s// /H}"
 
-      printf "%-$((5 * $(($length_total - $num_true_const))))s| $((100 * $(($num_true_const))/ $length_total))"; echo -en ' % '
+      printf "%-$((5 * $(($length_total - $num_true_const))))s| $((100 * $(($num_true_const))/ $length_total))"; echo -en ' % \r'
 
       if [ $num_true -ne $num_true_const ]
       then
@@ -3319,9 +3319,9 @@ function progress_bar(){
           do
               let "i=i+1"
               progress_unit=$progress_unit"H"
-              printf "\rProcessing WSO2 API Manager ... |"
+              printf "Processing WSO2 API Manager ... |"
               echo -n $progress_unit
-              printf "%-$((5 * $((${length_total} - ${num_true_const})) - $i))s| $(($(( 100 * $(($num_true_const))/ $length_total)) +  $((20 * $i/$length_total)) ))"; echo -en ' % '
+              printf "%-$((5 * $((${length_total} - ${num_true_const})) - $i))s| $(($(( 100 * $(($num_true_const))/ $length_total)) +  $((20 * $i/$length_total)) ))"; echo -en ' %\r '
               sleep 0.25
           done
           num_true_const=$num_true
@@ -3330,15 +3330,15 @@ function progress_bar(){
           let "time_proc=time_proc + 5"
       fi
 
-      printf "\rProcessing WSO2 API Manager ... |"
+      printf "Processing WSO2 API Manager ... |"
 
-      printf "%-$((5 * ${length_total-1}))s| $(($num_true_const * 100/ $length_total))"; echo -en ' % '
+      printf "%-$((5 * ${length_total-1}))s| $(($num_true_const * 100/ $length_total))"; echo -en ' %\r '
 
-      printf "\rProcessing WSO2 API Manager ... |"
+      printf "Processing WSO2 API Manager ... |"
       s=$(printf "%-$((5 * ${num_true_const}))s" "H")
       echo -en "${s// /H}"
 
-      printf "%-$((5 * $(($length_total - $num_true_const))))s| $((100 * $(($num_true_const))/ $length_total))"; echo -en ' % '
+      printf "%-$((5 * $(($length_total - $num_true_const))))s| $((100 * $(($num_true_const))/ $length_total))"; echo -en ' %\r '
 
       sleep 1
 
@@ -3393,22 +3393,29 @@ function deploy(){
     echoBold "1. Try navigating to https://$NODE_IP:30443/carbon/ from your favourite browser using \n"
     echoBold "\tusername: admin\n"
     echoBold "\tpassword: admin\n"
-    echoBold "2. Follow <getting-started-link> to start using WSO2 Identity Server.\n "
+    echoBold "2. Follow <getting-started-link> to start using WSO2 Identity Server.\n\n "
 }
 
 arg=$1
-case $arg in
-  -d|--deploy)
-    deploy
-    ;;
-  -u|--undeploy)
-    undeploy
-    ;;
-  -h|--help)
+arg=$1
+if [[ -z $arg ]]
+then
+    echoBold "Expected parameter is missing\n"
     usage
-    ;;
-  *)
-    echo "invalid argument"
-    usage
-    ;;
-esac
+else
+    case $arg in
+      -d|--deploy)
+        deploy
+        ;;
+      -u|--undeploy)
+        undeploy
+        ;;
+      -h|--help)
+        usage
+        ;;
+      *)
+        echoBold "Invalid parameter\n"
+        usage
+        ;;
+    esac
+fi
