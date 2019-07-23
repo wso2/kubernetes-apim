d4727a3f7b0c627c950f9d14c9532c53025c0aa6
Chiranga Alwis <chirangaalwis@gmail.com>
2019-07-23 14:16:51 +0530

    Make WSO2 API Manager's Key Manager profile the default Key Manager in WSO2 API Management deployment pattern 2

--- advanced/pattern-2/scripts/deploy.sh
+++ advanced/pattern-2/scripts/deploy.sh
@@ -29,7 +29,7 @@ function echoBold () {
     ${ECHO} -e $'\e[1m'"${1}"$'\e[0m'
 }
 
-read -p "Do you have a WSO2 Subscription? (Y/N)" -n 1 -r
+read -p "Do you have a WSO2 Subscription? (y/N)" -n 1 -r
 ${ECHO}
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
@@ -169,13 +169,13 @@ echoBold 'Creating Kubernetes ConfigMaps for WSO2 product configurations...'
 # create the APIM Analytics ConfigMaps
 ${KUBERNETES_CLIENT} create configmap apim-analytics-conf-worker --from-file=../confs/apim-analytics/
 # create the Kubernetes ConfigMaps for API Manager's KeyManager
-#${KUBERNETES_CLIENT} create configmap apim-km-conf --from-file=../confs/apim-km/
-#${KUBERNETES_CLIENT} create configmap apim-km-conf-axis2 --from-file=../confs/apim-km/axis2/
-#${KUBERNETES_CLIENT} create configmap apim-km-conf-datasources --from-file=../confs/apim-km/datasources/
+${KUBERNETES_CLIENT} create configmap apim-km-conf --from-file=../confs/apim-km/
+${KUBERNETES_CLIENT} create configmap apim-km-conf-axis2 --from-file=../confs/apim-km/axis2/
+${KUBERNETES_CLIENT} create configmap apim-km-conf-datasources --from-file=../confs/apim-km/datasources/
 # create the Kubernetes ConfigMaps for Identity Server as Key Manager
-${KUBERNETES_CLIENT} create configmap apim-is-as-km-conf --from-file=../confs/apim-is-as-km/
-${KUBERNETES_CLIENT} create configmap apim-is-as-km-conf-axis2 --from-file=../confs/apim-is-as-km/axis2/
-${KUBERNETES_CLIENT} create configmap apim-is-as-km-conf-datasources --from-file=../confs/apim-is-as-km/datasources/
+#${KUBERNETES_CLIENT} create configmap apim-is-as-km-conf --from-file=../confs/apim-is-as-km/
+#${KUBERNETES_CLIENT} create configmap apim-is-as-km-conf-axis2 --from-file=../confs/apim-is-as-km/axis2/
+#${KUBERNETES_CLIENT} create configmap apim-is-as-km-conf-datasources --from-file=../confs/apim-is-as-km/datasources/
 # create the Kubernetes ConfigMaps for API Manager's Publisher-Store-TM
 ${KUBERNETES_CLIENT} create configmap apim-pub-store-tm-1-conf --from-file=../confs/apim-pub-store-tm-1/
 ${KUBERNETES_CLIENT} create configmap apim-pub-store-tm-1-conf-datasources --from-file=../confs/apim-pub-store-tm-1/datasources/
@@ -196,12 +196,11 @@ ${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-persistent-volume-cla
 ${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-deployment.yaml
 # create a Kubernetes Service for MySQL
 ${KUBERNETES_CLIENT} create -f ../extras/rdbms/mysql/mysql-service.yaml
-sleep 30s
 
 echoBold 'Creating Kubernetes Services...'
 ${KUBERNETES_CLIENT} create -f ../apim-analytics/wso2apim-analytics-service.yaml
-#${KUBERNETES_CLIENT} create -f ../apim-km/wso2apim-km-service.yaml
-${KUBERNETES_CLIENT} create -f ../apim-is-as-km/wso2apim-is-as-km-service.yaml
+${KUBERNETES_CLIENT} create -f ../apim-km/wso2apim-km-service.yaml
+#${KUBERNETES_CLIENT} create -f ../apim-is-as-km/wso2apim-is-as-km-service.yaml
 ${KUBERNETES_CLIENT} create -f ../apim-pub-store-tm/wso2apim-pub-store-tm-1-service.yaml
 ${KUBERNETES_CLIENT} create -f ../apim-pub-store-tm/wso2apim-pub-store-tm-2-service.yaml
 ${KUBERNETES_CLIENT} create -f ../apim-pub-store-tm/wso2apim-pub-store-tm-service.yaml
@@ -212,32 +211,27 @@ ${KUBERNETES_CLIENT} create -f ../volumes/persistent-volumes.yaml
 
 echoBold 'Deploying WSO2 API Manager Analytics...'
 ${KUBERNETES_CLIENT} create -f ../apim-analytics/wso2apim-analytics-deployment.yaml
-sleep 1m
 
-#echoBold 'Deploying WSO2 API Manager Key Manager...'
-#${KUBERNETES_CLIENT} create -f ../apim-km/wso2apim-km-deployment.yaml
-#sleep 3m
+echoBold 'Deploying WSO2 API Manager Key Manager...'
+${KUBERNETES_CLIENT} create -f ../apim-km/wso2apim-km-deployment.yaml
 
-echoBold 'Deploying WSO2 Identity Server as Key Manager...'
-${KUBERNETES_CLIENT} create -f ../apim-is-as-km/wso2apim-is-as-km-volume-claim.yaml
-${KUBERNETES_CLIENT} create -f ../apim-is-as-km/wso2apim-is-as-km-deployment.yaml
-sleep 3m
+#echoBold 'Deploying WSO2 Identity Server as Key Manager...'
+#${KUBERNETES_CLIENT} create -f ../apim-is-as-km/wso2apim-is-as-km-volume-claim.yaml
+#${KUBERNETES_CLIENT} create -f ../apim-is-as-km/wso2apim-is-as-km-deployment.yaml
 
 echoBold 'Deploying WSO2 API Manager Publisher-Store-Traffic-Manager...'
 ${KUBERNETES_CLIENT} create -f ../apim-pub-store-tm/wso2apim-pub-store-tm-volume-claim.yaml
 ${KUBERNETES_CLIENT} create -f ../apim-pub-store-tm/wso2apim-pub-store-tm-1-deployment.yaml
-sleep 2m
 ${KUBERNETES_CLIENT} create -f ../apim-pub-store-tm/wso2apim-pub-store-tm-2-deployment.yaml
-sleep 3m
 
 echoBold 'Deploying WSO2 API Manager Gateway...'
 ${KUBERNETES_CLIENT} create -f ../apim-gw/wso2apim-gateway-volume-claim.yaml
 ${KUBERNETES_CLIENT} create -f ../apim-gw/wso2apim-gateway-deployment.yaml
-sleep 4m
 
 echoBold 'Deploying Kubernetes Ingresses...'
 ${KUBERNETES_CLIENT} create -f ../ingresses/wso2apim-gateway-ingress.yaml
 ${KUBERNETES_CLIENT} create -f ../ingresses/wso2apim-ingress.yaml
+sleep 5m
 
 echoBold 'Finished'
 echo 'To access the WSO2 API Manager Management console, try https://wso2apim/carbon in your browser.'
