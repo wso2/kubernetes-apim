252478591cb2557f9c96618e46c890efcd5b391f
Renuka Fernando <renukapiyumal@gmail.com>
2020-01-08 14:04:18 +0530

    Fix if condition in deployment shell script

--- simple/deployment-scripts/wso2am-latest.sh
+++ simple/deployment-scripts/wso2am-latest.sh
@@ -3993,7 +3993,7 @@ function get_node_ip(){
 
   if [[ -z $NODE_IP ]]
   then
-      if [[ $(kubectl config current-context)="minikube" ]]
+      if [[ $(kubectl config current-context) == "minikube" ]]
       then
           NODE_IP=$(minikube ip)
       else
